a29d2ede9de68db8c6be27631a4e65d1bc653b2d
Will Alread <102969295+walread@users.noreply.github.com>
2022-06-26 14:19:09 +0000

    Sun, Jun 26, 2022, 2:19 PM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -24,10 +24,9 @@ def main():
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
-    for wins in range(N):
+    for i in range(N):
         winner = simulate_tournamnet(teams)
         if winner in counts:
-
         else:
             counts[team_name] = winner
 
