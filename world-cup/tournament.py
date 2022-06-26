74c9a8db169bc03464ef478175abcf7a03e9e767
Will Alread <102969295+walread@users.noreply.github.com>
2022-06-26 13:32:20 +0000

    Sun, Jun 26, 2022, 1:32 PM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -24,6 +24,7 @@ def main():
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
+    simulate_tournament(teams) 
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
