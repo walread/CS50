13aacbf0a41d10efdbef9664fa264f26d10211c5
Will Alread <102969295+walread@users.noreply.github.com>
2022-06-27 12:45:30 +0000

    Mon, Jun 27, 2022, 12:45 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,2 +1,9 @@
 def main():
-    height = get_int("Height: ")
+    height = get_height()
+    for i in range(height):
+        print("#")
+
+def get_height():
+    while True:
+        try:
+            n = int(input("Height: "))
