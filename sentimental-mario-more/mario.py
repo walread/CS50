1bc705b634bbf59aa4fc5fe5f12636f00777f331
Will Alread <102969295+walread@users.noreply.github.com>
2022-06-27 22:05:10 +0000

    Mon, Jun 27, 2022, 10:05 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -4,7 +4,7 @@ def main():
     for i in range(n):
         for j in range(i + 1):
             print(" ", end="")
-        for k in range():
+        for k in range(i + 1):
             print("#", end="")
         print(" ", end="")
         for l in range(i + 1):
