7fbdec906c0db3b575b16d286aec22297a19e0ac
Will Alread <102969295+walread@users.noreply.github.com>
2022-06-28 01:47:07 +0000

    Tue, Jun 28, 2022, 1:47 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -3,10 +3,4 @@ from cs50 import get_string
 s = get_string("Number: ")
 n = int(s)
 if (s[0] == 3):
-        print("AMEX")
-elif (n):
-    print("MASTERCARD")
-elif (n):
-    print("VISA")
-else:
-    print("INVALID")
+    print("AMEX")
