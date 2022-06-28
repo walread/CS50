12f1e731b3d916735390d79fa0ee1acca5a06f26
Will Alread <102969295+walread@users.noreply.github.com>
2022-06-28 10:58:35 +0000

    Tue, Jun 28, 2022, 10:58 AM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -5,9 +5,13 @@ from cs50 import get_string
 t = get_string("Text: ")
 
 
+
 #calculate grade level
-L = len(t)
-S =
+for i in range(len(t)):
+    if (t[i] == )
+
+L = 0
+S = 0
 n = (0.0588 * L) - (0.296 * S) - 15.8
 n = round(n)
 
