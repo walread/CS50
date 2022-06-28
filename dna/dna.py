e29494b8c6e3ad1bfe614bee3c3d65fc3942e224
Will Alread <102969295+walread@users.noreply.github.com>
2022-06-28 12:56:19 +0000

    Tue, Jun 28, 2022, 12:56 PM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -6,7 +6,7 @@ def main():
 
     # TODO: Check for command-line usage
     if len(sys.argv) != 3:
-        sys.exit("Usage: python dna.py databases/DATABASE.csv sequences/SEQUENCE.txt")
+        sys.exit("Usage: python dna.py data.csv sequence.txt")
 
     # TODO: Read database file into a variable
     database = []
