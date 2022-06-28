bc64d822d5aaa3d910b9323caf430ce598c473ad
Will Alread <102969295+walread@users.noreply.github.com>
2022-06-28 01:50:07 +0000

    Tue, Jun 28, 2022, 1:50 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -2,5 +2,5 @@ from cs50 import get_string
 
 s = get_string("Number: ")
 if (int(s[0]) == 3):
-    if (int(s[1]) )
-    print("AMEX")
+    if (int(s[1]) == 4 or int(s[1]) == 7):
+        print("AMEX")
