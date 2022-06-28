1f8ad3310bd2dad049160f1b07d4b0cb7024f704
Will Alread <102969295+walread@users.noreply.github.com>
2022-06-28 01:35:09 +0000

    Tue, Jun 28, 2022, 1:35 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -2,7 +2,7 @@ from cs50 import get_string
 
 s = get_string("Number: ")
 n = int(s)
-if (s[0] = 3 and s[1] = 4 or 7):
+if (s[0] == 3 and (s[1] == 4 or s[1] == 7)):
     print("AMEX")
 elif (n):
     print("MASTERCARD")
