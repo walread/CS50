cf80a7c8c0eb7ec84a0fffd403427cacf317a2d4
Will Alread <102969295+walread@users.noreply.github.com>
2022-06-28 10:41:47 +0000

    Tue, Jun 28, 2022, 10:41 AM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -5,3 +5,14 @@ from cs50 import get_string
 s = get_string("Text: ")
 
 
+#calculate grade level
+n = 2
+
+
+# print grade level
+if (n < 1):
+    print("Before Grade 1")
+elif (n >= 16):
+    print("Grade 16+")
+else:
+    print(f"Grade {n}")
