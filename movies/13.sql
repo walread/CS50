09e92fde553ea86b38505fd4fedb62f124e28a0a
Will Alread <102969295+walread@users.noreply.github.com>
2022-07-06 16:02:19 +0000

    Wed, Jul 6, 2022, 4:02 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,2 +1,4 @@
 SELECT name FROM people WHERE id IN
-(SELECT )
+(SELECT person_id FROM stars WHERE movie_id =
+(SELECT movie_id FROM stars WHERE person_id =
+(SELECT id FROM people WHERE name = 'Kevin Bacon')));
