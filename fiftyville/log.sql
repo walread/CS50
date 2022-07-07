3ae7cd01684e2dc3877d860cd53f908d9d2f5818
Will Alread <102969295+walread@users.noreply.github.com>
2022-07-07 11:09:37 +0000

    Thu, Jul 7, 2022, 11:09 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+SELECT * FROM crime_scene_reports WHERE street = 'Humphrey Street' and day = 28;
+-- crime id of interest is 295 
