2dca16686b48073d3ad9a90c943218aeed12db18
Will Alread <102969295+walread@users.noreply.github.com>
2022-07-07 01:26:29 +0000

    Thu, Jul 7, 2022, 1:26 AM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,4 +1,5 @@
 SELECT DISTINCT name FROM people WHERE id IN
 (SELECT person_id FROM stars WHERE movie_id IN
 (SELECT movie_id FROM stars WHERE person_id =
-(SELECT id FROM people WHERE name = 'Kevin Bacon' and birth = 1958)));
+(SELECT id FROM people WHERE name = 'Kevin Bacon' and birth = 1958)))
+AND name != 'Kevin Bacon';
