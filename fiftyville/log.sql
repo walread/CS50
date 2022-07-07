5d7b79237477a9e231d21916a5e9153fef9c0b53
Will Alread <102969295+walread@users.noreply.github.com>
2022-07-07 11:21:51 +0000

    Thu, Jul 7, 2022, 11:21 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,12 +1,14 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- find crime of interest
 SELECT *
 FROM crime_scene_reports
 WHERE street = 'Humphrey Street'
 AND day = 28;
--- crime of interest is id 295
+-- id = 295
 
+-- look at interviews
 SELECT *
 FROM bakery_security_logs
 WHERE id = 295;
--- license plate is IH61G08, but day is 29th rather than 28th... 
+-- license plate is IH61G08, but day is 29th rather than 28th...
