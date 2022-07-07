ca2b36bcddba9c697e89b508d1d16a3a49c2493c
Will Alread <102969295+walread@users.noreply.github.com>
2022-07-07 11:30:20 +0000

    Thu, Jul 7, 2022, 11:30 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,16 +1,18 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
--- find crime of interest
+-- Find crime of interest.
 SELECT *
 FROM crime_scene_reports
 WHERE street = 'Humphrey Street'
 AND day = 28;
 -- Id = 295. Took place at 10:15am. Three interviews conducted.
 
+-- Look at interviews.
+
+
 -- Identify license plate #.
 SELECT *
 FROM bakery_security_logs
-WHERE id = 295;
--- license plate # is 
-
--- Look at interviews.
+WHERE month = 7
+AND day = 28;
+-- license plate # is
