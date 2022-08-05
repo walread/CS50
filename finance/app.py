1c038ffd6b20a9768fc0f1d5577dc31265add210
Will Alread <102969295+walread@users.noreply.github.com>
2022-08-05 11:47:18 +0000

    Fri, Aug 5, 2022, 11:47 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -135,6 +135,8 @@ def register():
         elif len(db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))) != 0:
             return apology("username is already taken", 403)
 
+        werkzeug.security.generate_password_hash(password, method='pbkdf2:sha256', salt_length=8) 
+
     else:
         return render_template("register.html")
 
