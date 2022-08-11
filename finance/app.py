cda6126e8b060fc38b6a35037496aa254398f4fd
Will Alread <102969295+walread@users.noreply.github.com>
2022-08-11 10:40:53 +0000

    Thu, Aug 11, 2022, 10:40 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -46,15 +46,14 @@ def index():
 
     user_id = session["user_id"]
 
-    holdings = db.execute("SELECT symbol, name, SUM(shares) AS shares, price FROM transactions WHERE user_id = ? GROUP BY symbol", user_id)
+    holdings = db.execute("SELECT symbol, name, SUM(shares) AS shares_sum, price FROM transactions WHERE user_id = ? GROUP BY symbol", user_id)
 
     cash = db.execute("SELECT cash FROM users WHERE id = ?", user_id)[0]["cash"]
 
     total = cash
 
     for holding in holdings:
-        price = lookup(holding["symbol"])
-        total += holdings["price"] * holding["shares"]
+        total += holding["price"] * holding["shares_sum"]
 
     return render_template("index.html", holdings = holdings, cash = cash, total = total)
 
