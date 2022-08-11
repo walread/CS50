251a9cb0b69db068a445d7616dac82871b3c2d64
Will Alread <102969295+walread@users.noreply.github.com>
2022-08-11 11:59:38 +0000

    Thu, Aug 11, 2022, 11:59 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -48,8 +48,6 @@ def index():
 
     holdings = db.execute("SELECT symbol, name, SUM(shares) AS shares_sum, price FROM transactions WHERE user_id = ? GROUP BY symbol", user_id)
 
-    current_price = lookup(holdings["symbol"])
-
     cash = db.execute("SELECT cash FROM users WHERE id = ?", user_id)[0]["cash"]
 
     total = cash
@@ -58,7 +56,7 @@ def index():
         info = lookup(holding["symbol"])
         total += info["price"] * holding["shares_sum"]
 
-    return render_template("index.html", holdings = holdings, current_price = current_price, cash = cash, total = total)
+    return render_template("index.html", holdings = holdings, cash = cash, total = total)
 
 
 @app.route("/buy", methods=["GET", "POST"])
