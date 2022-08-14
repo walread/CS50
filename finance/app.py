8cd82c191e36ab903b0abd48711ffe8d312f50e6
Will Alread <102969295+walread@users.noreply.github.com>
2022-08-14 15:24:43 +0000

    Sun, Aug 14, 2022, 3:24 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -49,7 +49,8 @@ def index():
     holdings = db.execute("SELECT symbol, name, SUM(shares) AS shares_sum, price FROM transactions WHERE user_id = ? GROUP BY symbol", user_id)
 
     for holding in holdings:
-        if holding[]
+        if holding["shares_SUM"] = 0:
+            db.execute(DELETE FROM transactions WHERE user_id = ? )
         current_price = lookup(holding["symbol"])
 
     cash = db.execute("SELECT cash FROM users WHERE id = ?", user_id)[0]["cash"]
