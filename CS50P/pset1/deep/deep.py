e484a39f3f9f5b486115d75b1933910f1f4d0a79
Will Alread <102969295+walread@users.noreply.github.com>
2022-08-19 21:47:13 +0000

    ddd, Aug 8/19/2022, YYYY, 9:47 A +0

---
+++ CS50P/pset1/deep/deep.py
@@ -0,0 +1,6 @@
+answer = input("What is the answer to the Great Question of Life, the Universe and Everything? ")
+
+if answer == 42 or answer.lower() == "forty two" or answer.lower() == "forty-two":
+    print("Yes")
+else:
+    print("No")
