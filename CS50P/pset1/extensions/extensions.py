2184bc3fefaff10453cdc39be87b95050c62a9ba
Will Alread <102969295+walread@users.noreply.github.com>
2022-08-20 13:26:33 +0000

    ddd, Aug 8/20/2022, YYYY, 1:26 A +0

--- CS50P/pset1/extensions/extensions.py
+++ CS50P/pset1/extensions/extensions.py
@@ -1,6 +1,16 @@
 file = input("File name: ").lower().strip()
 
-if ".jpg" in file or ".jpeg" in file:
+if ".gif" in file:
+    print("image/gif")
+elif ".jpg" in file or ".jpeg" in file:
     print("image/jpeg")
+elif ".png" in file:
+    print("image/png")
+elif ".pdf" in file:
+    print("application/pdf")
+elif ".txt" in file:
+    print("application/txt")
+elif ".zip" in file:
+    print("")
 else:
     print("applicaton/octet-stream")
