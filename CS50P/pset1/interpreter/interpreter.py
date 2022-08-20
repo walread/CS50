1934d04c5745f50e5736f81c048a091a3d15241f
Will Alread <102969295+walread@users.noreply.github.com>
2022-08-20 14:06:53 +0000

    ddd, Aug 8/20/2022, YYYY, 2:06 A +0

--- CS50P/pset1/interpreter/interpreter.py
+++ CS50P/pset1/interpreter/interpreter.py
@@ -3,6 +3,13 @@ x, y, z = input("Expression: ").split(" ")
 x = float(x)
 z = float(z)
 
-answer = (x + z)
+if y == "+":
+    answer = x + z
+elif y == "-":
+    answer = x - z
+elif y == "*":
+    answer = x * z
+elif y == "/":
+    answer = x / z
 
-print(answer)
+print(.2f{answer}))
