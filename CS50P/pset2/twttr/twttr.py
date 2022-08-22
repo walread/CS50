4b2415f9d84a552eed0c0d78984e431a95d28a1b
Will Alread <102969295+walread@users.noreply.github.com>
2022-08-22 16:00:13 +0000

    ddd, Aug 8/22/2022, YYYY, 4:00 A +0

--- CS50P/pset2/twttr/twttr.py
+++ CS50P/pset2/twttr/twttr.py
@@ -1,5 +1,7 @@
 input = input("Input: ")
 
 for c in input:
-    if c.upper() == "A" or c.upper() == "E" or c.upper() == "I" or c.upper() == "O" or c.upper() == "U"
-    
+    if c.upper() == "A" or c.upper() == "E" or c.upper() == "I" or c.upper() == "O" or c.upper() == "U":
+        input = input["c"].replace("")
+
+print("Output:", input)
