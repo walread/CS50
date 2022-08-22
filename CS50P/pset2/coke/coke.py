9ebf8edd151be88f8a483ac8a8e50a7becf074f0
Will Alread <102969295+walread@users.noreply.github.com>
2022-08-22 15:42:31 +0000

    ddd, Aug 8/22/2022, YYYY, 3:42 A +0

--- CS50P/pset2/coke/coke.py
+++ CS50P/pset2/coke/coke.py
@@ -1,6 +1,9 @@
 due = 50
 
-print("Amount Due:", due)
+while due != 0:
+    print("Amount Due:", due)
+    coin = int(input("Insert Coin: "))
+    if coin == 25 or coin == 10 or coin == 5:
+        due -= coin
 
-coin = input("Insert Coin: ")
-
+print("Change Owed: ", )
