7c6eb1462e091f57fb2c8bb5e698d6559d6dc05e
Will Alread <102969295+walread@users.noreply.github.com>
2022-08-23 11:23:02 +0000

    ddd, Aug 8/23/2022, YYYY, 11:23 A +0

--- CS50P/pset2/plates/plates.py
+++ CS50P/pset2/plates/plates.py
@@ -11,12 +11,14 @@ def is_valid(s):
     if len(s) > 6:
         return False
     else:
-        if s.isalnum():
-            if s[0:1].isalpha:
+        if s[0:1].isalpha():
+            if s.isalnum:
                 t = ""
                 for c in s:
                     if c.isdigit():
                         t += c
+                if t.isdigit():
+                    return True
             else:
                 return False
         else:
