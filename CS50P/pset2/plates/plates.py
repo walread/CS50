e33bd710d2fcced010c5364be396e072fe26d876
Will Alread <102969295+walread@users.noreply.github.com>
2022-08-23 10:51:30 +0000

    ddd, Aug 8/23/2022, YYYY, 10:51 A +0

--- CS50P/pset2/plates/plates.py
+++ CS50P/pset2/plates/plates.py
@@ -8,10 +8,17 @@ def main():
 
 def is_valid(s):
 
-    if len(s) != 6:
+    if len(s) <= 6:
         return False
     else:
         if s.isalnum():
+            if s[0, 1].isalpha:
+                for c in s:
+                    if c.
+            else:
+                return False
+        else:
+            return False
 
 
 main()
