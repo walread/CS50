a686f644a5c7be146f2b1eb8a66f1837c0c457f8
Will Alread <102969295+walread@users.noreply.github.com>
2022-08-23 10:20:13 +0000

    ddd, Aug 8/23/2022, YYYY, 10:20 A +0

--- CS50P/pset2/twttr/twttr.py
+++ CS50P/pset2/twttr/twttr.py
@@ -1,19 +1,8 @@
 input = input("Input: ")
 
-vowel = "AEIOUaeiou
+vowel = "AEIOUaeiou"
 
-for c in input:
-    for d in vowel:
-
-input = input.replace("a", "")
-input = input.replace("e", "")
-input = input.replace("i", "")
-input = input.replace("o", "")
-input = input.replace("u", "")
-input = input.replace("A", "")
-input = input.replace("E", "")
-input = input.replace("I", "")
-input = input.replace("O", "")
-input = input.replace("U", "")
+for c in vowel:
+    input = input.replace(c, "")
 
 print("Output:", input)
