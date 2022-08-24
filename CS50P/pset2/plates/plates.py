216b9fc2f9c06e741dc041ce65356daf7a9b8205
Will Alread <102969295+walread@users.noreply.github.com>
2022-08-24 10:46:12 +0000

    ddd, Aug 8/24/2022, YYYY, 10:46 A +0

--- CS50P/pset2/plates/plates.py
+++ CS50P/pset2/plates/plates.py
@@ -5,15 +5,27 @@ def main():
     else:
         print("Invalid")
 
+# must be 2-6 characters, all alphanumeric
+# characters 1-2 must be alpha
+# if there a numbers, letters must not follow
+
 # use while true loop to split string (while string contains digits, split string)
 # could try to insert space once number is reached to split string without loosing first number
 # break while loop once first number is reached
-# use try, except, else to overcome assignment error when spliting 
+# use try, except, else to overcome assignment error when spliting
 
 def is_valid(s):
 
-    if len(s) <= 6:
-        if s[0:1].isalpha():
+    if len(s) > 6 or len(s) < 2:
+        return False
+    elif not s.isalnum:
+        return False
+    elif not s[0:1].isalpha():
+        return False
+    elif:
+
+
+
             t = ""
             for c in s:
                 if c.isdigit():
@@ -21,10 +33,6 @@ def is_valid(s):
                     break
             if u.isdigit() or len(u) == 0:
                 return True
-        else:
-            return False
-    else:
-        return False
 
 
 main()
