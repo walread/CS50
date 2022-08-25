a84f1854e28a1c3e34f9ac6d79cd7fd00c8b8bea
Will Alread <102969295+walread@users.noreply.github.com>
2022-08-25 10:42:44 +0000

    ddd, Aug 8/25/2022, YYYY, 10:42 A +0

--- CS50P/pset3/fuel/fuel.py
+++ CS50P/pset3/fuel/fuel.py
@@ -1,4 +1,15 @@
-fraction = input("Fraction: ")
+while True:
+    try:
+        fraction = input("Fraction: ")
+        x, y = fraction.split("/")
+        amount = float(x) / float(y) * 100
+        break
+    except ValueError, ZeroDivisionError:
+        pass
 
-try:
-    x, y = fraction.split("/") 
+if amount >= 99:
+    print("F")
+elif amount <= 1:
+    print("E")
+else:
+    print(amount) 
