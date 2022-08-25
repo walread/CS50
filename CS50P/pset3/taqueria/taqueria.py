bcb7a5037ca3648547245d777643037fccfb24ba
Will Alread <102969295+walread@users.noreply.github.com>
2022-08-25 12:20:05 +0000

    ddd, Aug 8/25/2022, YYYY, 12:20 A +0

--- CS50P/pset3/taqueria/taqueria.py
+++ CS50P/pset3/taqueria/taqueria.py
@@ -18,13 +18,10 @@ while True:
         if item in entrees:
             total += entrees[item]
     except EOFError:
-        if item in entrees:
-            total += entrees[item]
-        print()
         break
     except KeyError:
         pass
 
 total = "{:.2f}".format(total)
-
+print()
 print(f"Total: ${total}")
