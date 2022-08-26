1a837bc62221f89ca5334e134bd859f7d3a697cc
Will Alread <102969295+walread@users.noreply.github.com>
2022-08-26 11:34:03 +0000

    ddd, Aug 8/26/2022, YYYY, 11:34 A +0

--- CS50P/pset3/outdated/outdated.py
+++ CS50P/pset3/outdated/outdated.py
@@ -1,7 +1,7 @@
 while True:
     try:
         date = input("Date: ")
-        month, day, year = date.split("/" or "," or " ")
+        month, day, year, space = date.split("/" or "," or " ")
         break
     except ValueError:
         pass
@@ -12,3 +12,4 @@ print(year)
 
 
 # might need to do multipule if statements under try before spliting (if "/" in date, then split on "/")
+# give four variable to date with spaces
