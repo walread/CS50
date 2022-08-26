ec8f6660e7f1c63798dae5fc75e23ee8a30c3fee
Will Alread <102969295+walread@users.noreply.github.com>
2022-08-26 11:07:31 +0000

    ddd, Aug 8/26/2022, YYYY, 11:07 A +0

--- CS50P/pset3/grocery/grocery.py
+++ CS50P/pset3/grocery/grocery.py
@@ -4,4 +4,13 @@ grocery = {
 
 while True:
     try:
-        item = input()
+        item = input().UPPER()
+        if item in grocery:
+            grocery[item] += 1
+        else:
+            grocery[item] = 1
+    except EOFError:
+        print()
+        break
+
+for item in grocery:
