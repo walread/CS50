cb47f5c9944e64d20dd653e9bca2b537ce250147
Will Alread <102969295+walread@users.noreply.github.com>
2022-08-27 13:32:55 +0000

    ddd, Aug 8/27/2022, YYYY, 1:32 A +0

--- CS50P/pset3/outdated/outdated.py
+++ CS50P/pset3/outdated/outdated.py
@@ -16,16 +16,17 @@ months = {
 while True:
     try:
         date = input("Date: ").strip()
-        if "/" in date:
-            month, day, year = date.split("/")
-            month = int(month)
-            day = int(day)
-        elif "," in date:
-            month, day, year = date.split(" ")
-            month = int(months[month.title()])
-            day = int(day.replace(",", ""))
-        if month <= 12 and day <= 31:
-            break
+        if "/" or "," in date:
+            if "/" in date:
+                month, day, year = date.split("/")
+                month = int(month)
+                day = int(day)
+            elif "," in date:
+                month, day, year = date.split(" ")
+                month = int(months[month.title()])
+                day = int(day.replace(",", ""))
+            if month <= 12 and day <= 31:
+                break
     except (ValueError, KeyError):
         pass
 
