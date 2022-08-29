9f31937ff69d5adf9016af067625b2e67fd66790
Will Alread <102969295+walread@users.noreply.github.com>
2022-08-29 21:57:12 +0000

    ddd, Aug 8/29/2022, YYYY, 9:57 A +0

--- CS50P/pset4/figlet/figlet.py
+++ CS50P/pset4/figlet/figlet.py
@@ -4,6 +4,11 @@ import sys
 if len(sys.argv) != 3:
     sys.exit
 else:
+    f = sys.argv[1]
+    s = sys.argv[2]
     figlet = Figlet()
-    figlet.getFonts()
-    figlet.setFont(font=f)
+    if f not in figlet.getFonts():
+        sys.exit("Font not in font list")
+    else:
+        figlet.setFont(font = f)
+        print(figlet.renderText(s))
