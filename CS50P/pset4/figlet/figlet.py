b19d10393fd670571fbdb84087a91b631f25dc6e
Will Alread <102969295+walread@users.noreply.github.com>
2022-08-31 13:32:11 +0000

    ddd, Aug 8/31/2022, YYYY, 1:32 A +0

--- CS50P/pset4/figlet/figlet.py
+++ CS50P/pset4/figlet/figlet.py
@@ -8,18 +8,18 @@ font_list = figlet.getFonts()
 if len(sys.argv) == 1:
     s = input("Input: ")
     f = random.choice(font_list)
-    figlet.setFont(font = f)
+    figlet.setFont(font=f)
     print(figlet.renderText(s))
 elif len(sys.argv) == 3:
     e = sys.argv[1]
     f = sys.argv[2]
     if e != "-f":
-        sys.exit(e)
+        sys.exit("Invalid usage")
     elif f not in font_list:
         sys.exit("Invalid usage")
     else:
         s = input("Input: ")
-        figlet.setFont(font = f)
+        figlet.setFont(font=f)
         print(figlet.renderText(s))
 else:
     sys.exit("Invalid usage")
