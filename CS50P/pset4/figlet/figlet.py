7ed12a73eb2362f59d91b43481832d2bc5abf08b
Will Alread <102969295+walread@users.noreply.github.com>
2022-08-31 13:22:40 +0000

    ddd, Aug 8/31/2022, YYYY, 1:22 A +0

--- CS50P/pset4/figlet/figlet.py
+++ CS50P/pset4/figlet/figlet.py
@@ -14,12 +14,12 @@ elif len(sys.argv) == 3:
     e = sys.argv[1]
     f = sys.argv[2]
     if e != "-f" or e != "--font":
-        sys.exit("First argument is -f or --font")
+        sys.exit("Invalid usage")
     elif f not in font_list:
-        sys.exit("Second argument is font_name")
+        sys.exit("Invalid usage")
     else:
         s = input("Input: ")
         figlet.setFont(font = f)
         print(figlet.renderText(s))
 else:
-    sys.exit("Zero or two command line arguments")
+    sys.exit("Invalid usage") 
