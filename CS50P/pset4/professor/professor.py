701f3751fd9c3d3216c5846fe833f51c88f7a3cc
Will Alread <102969295+walread@users.noreply.github.com>
2022-09-01 19:08:24 +0000

    ddd, Sep 9/1/2022, YYYY, 7:08 A +0

--- CS50P/pset4/professor/professor.py
+++ CS50P/pset4/professor/professor.py
@@ -2,11 +2,17 @@ import random
 
 
 def main():
-    ...
+    get_level()
 
 
 def get_level():
-    ...
+    while True:
+        try:
+            level = int(input("Level: "))
+            if level in range(1, 3):
+                break
+        except ValueError:
+            pass
 
 
 def generate_integer(level):
