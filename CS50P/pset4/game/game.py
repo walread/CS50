520dede6be273ca6d332a349e5a30979c8b96cba
Will Alread <102969295+walread@users.noreply.github.com>
2022-09-01 18:30:06 +0000

    ddd, Sep 9/1/2022, YYYY, 6:30 A +0

--- CS50P/pset4/game/game.py
+++ CS50P/pset4/game/game.py
@@ -4,20 +4,9 @@ while True:
     try:
         n = int(input("Level: "))
         if n > 0:
-            level = random.randint(1, int(n))
-            print(level)
-
+            break
     except ValueError:
         pass
 
-while True:
-    guess = input("Guess: ")
-    if guess.isdigit():
-        guess = int(guess)
-        if guess > level:
-            print("Too large!")
-        elif guess < level:
-            print("Too small")
-        else:
-            print("Just right!")
-            break
+level = random.randint(1, int(n))
+print(level)
