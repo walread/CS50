fc87a2a8848c8a3fef52138b3524c26d8c49820e
Will Alread <102969295+walread@users.noreply.github.com>
2022-09-01 19:21:03 +0000

    ddd, Sep 9/1/2022, YYYY, 7:21 A +0

--- CS50P/pset4/professor/professor.py
+++ CS50P/pset4/professor/professor.py
@@ -3,6 +3,13 @@ import random
 
 def main():
     level = get_level()
+    x = generate_integer(level)
+    y = generate_integer(level)
+    answer = int(input(f"{x} + {y} = "))
+    if answer == (x + y):
+        print("Correct")
+    else:
+        print("EEE")
 
 
 def get_level():
@@ -16,7 +23,12 @@ def get_level():
 
 
 def generate_integer(level):
-    ...
+    if level == 1:
+        return random.randint(0, 9)
+    elif level == 2:
+        return random.randint(10, 99)
+    elif level == 3:
+        return random.randint(100, 999)
 
 
 if __name__ == "__main__":
