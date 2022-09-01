110584ec76f208d2f112ae1b3b762facf5df942d
Will Alread <102969295+walread@users.noreply.github.com>
2022-09-01 18:22:39 +0000

    ddd, Sep 9/1/2022, YYYY, 6:22 A +0

--- CS50P/pset4/game/game.py
+++ CS50P/pset4/game/game.py
@@ -3,22 +3,11 @@ import random
 while True:
     n = input("Level: ")
     try:
-        int(n) > 0
-        break
+        n = int(n)
+        if n > 0:
+            level = random.randint(1, int(n))
+            print(level)
+            break
     except ValueError:
         pass
 
-level = random.randint(1, int(n))
-print(level)
-
-while True:
-    guess = input("Guess: ")
-    if guess.isdigit():
-        guess = int(guess)
-        if guess > level:
-            print("Too large!")
-        elif guess < level:
-            print("Too small")
-        else:
-            print("Just right!")
-            break
