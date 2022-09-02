224c100fcb51849a605f60f528b444fd52d01980
Will Alread <102969295+walread@users.noreply.github.com>
2022-09-02 10:59:47 +0000

    ddd, Sep 9/2/2022, YYYY, 10:59 A +0

--- CS50P/pset4/bitcoin/bitcoin.py
+++ CS50P/pset4/bitcoin/bitcoin.py
@@ -11,7 +11,7 @@ else:
 
 try:
     r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-    rate = r.json()["bpi"]["USD"]["rate"]
-    print(f"${rate * int(sys.argv[1])}") 
+    rate = r.json()["bpi"]["USD"]["rate_float"]
+    print(f"${rate * float(sys.argv[1])}")
 except requests.RequestException:
     sys.exit("Decoding fail")
