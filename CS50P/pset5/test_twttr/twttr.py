1fe0788902d154c91af605b80abbc71fd2885c38
Will Alread <102969295+walread@users.noreply.github.com>
2022-09-02 11:22:57 +0000

    ddd, Sep 9/2/2022, YYYY, 11:22 A +0

--- CS50P/pset5/test_twttr/twttr.py
+++ CS50P/pset5/test_twttr/twttr.py
@@ -1,9 +1,13 @@
 def main():
-    ...
+    s = input("Input: ")
+    print("Output:", shorten(s))
 
 
 def shorten(word):
-    ...
+    vowel = "AEIOUaeiou"
+    for c in vowel:
+        word = word.replace(c, "")
+    return word
 
 
 if __name__ == "__main__":
