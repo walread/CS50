dc75874a349902147fc915d1b6aaf4c161686e56
Will Alread <102969295+walread@users.noreply.github.com>
2022-09-02 23:19:59 +0000

    ddd, Sep 9/2/2022, YYYY, 11:19 A +0

--- CS50P/pset5/test_twttr/test_twttr.py
+++ CS50P/pset5/test_twttr/test_twttr.py
@@ -1,11 +1,4 @@
 from twttr import shorten
 
-def test_lower():
-    vowel = "AEIOUaeiou"
-    for c in vowel:
-        assert c not in shorten("cat")
-
-def test_upper():
-    vowel = "AEIOUaeiou"
-    for c in vowel:
-        assert c not in shorten("CAT")
+def test_shorten():
+    assert shorten("1Apple!") == ("1ppl!")
