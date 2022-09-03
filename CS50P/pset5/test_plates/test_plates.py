c8ce4c87cf9017e9a4c02143ab76bab3f2ef9807
Will Alread <102969295+walread@users.noreply.github.com>
2022-09-03 11:58:58 +0000

    ddd, Sep 9/3/2022, YYYY, 11:58 A +0

--- CS50P/pset5/test_plates/test_plates.py
+++ CS50P/pset5/test_plates/test_plates.py
@@ -1,4 +1,15 @@
 from plates import is_valid
 
 
-def test_
+def test_start():
+    assert is_valid("11AAAA") == False
+
+def test_length():
+    assert is_valid("A") == False
+    assert is_valid("AAAAAAA") == False
+
+def test_numbers():
+    assert is_valid("AA00AA") == False
+
+def test_punct():
+    assert is_valid("A!A. A") == False
