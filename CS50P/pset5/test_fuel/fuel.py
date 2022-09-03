0de692a6a36d0e645561ca04b0b6b4fd82bd462b
Will Alread <102969295+walread@users.noreply.github.com>
2022-09-03 12:48:58 +0000

    ddd, Sep 9/3/2022, YYYY, 12:48 A +0

--- CS50P/pset5/test_fuel/fuel.py
+++ CS50P/pset5/test_fuel/fuel.py
@@ -1,20 +1,5 @@
 def main():
-    ...
-
-
-def convert(fraction):
-    ...
-
-
-def gauge(percentage):
-    ...
-
-
-if __name__ == "__main__":
-    main()
-
-
-while True:
+    while True:
     try:
         fraction = input("Fraction: ")
         x, y = fraction.split("/")
@@ -30,3 +15,15 @@ elif amount <= 1:
     print("E")
 else:
     print(f"{amount}%")
+
+
+def convert(fraction):
+    ...
+
+
+def gauge(percentage):
+    ...
+
+
+if __name__ == "__main__":
+    main()
