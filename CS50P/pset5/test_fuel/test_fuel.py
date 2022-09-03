a24939ccde443a245ab88388d8982ff1374a3c98
Will Alread <102969295+walread@users.noreply.github.com>
2022-09-03 13:14:16 +0000

    ddd, Sep 9/3/2022, YYYY, 1:14 A +0

--- CS50P/pset5/test_fuel/test_fuel.py
+++ CS50P/pset5/test_fuel/test_fuel.py
@@ -1,8 +1,8 @@
 import fuel
 
 def test_convert():
-    assert fuel.convert("1/10") == 0.1
+    assert fuel.convert("1/10") == 10
 
 
 def test_gauge():
-    assert fuel.gauge(0.5) == "50%"
+    assert fuel.gauge(50) == "50%"
