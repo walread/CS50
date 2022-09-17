3e74e80059f4db97103c826727ce644f115a7477
walread <will.alread@gmail.com>
2022-09-17 23:15:45 +0000

    Sat, Sep 17, 2022, 11:15 PM +00:00

--- CS50P/pset6/pizza/pizza.py
+++ CS50P/pset6/pizza/pizza.py
@@ -3,9 +3,20 @@ from tabulate import tabulate
 
 pizzas = []
 
-with open(sys.argv[1]) as file:
-    reader = csv.DictReader(file)
+if len(sys.argv) < 2:
+    sys.exit("Too few command-line arguments")
+elif len(sys.argv) > 2:
+    sys.exit("Too many command-line arguments")
+name, type = sys.argv[1].split(".")
+if type != "csv":
+    sys.exit("Not a CSV file")
+
+try:
+    with open(sys.argv[1]) as file:
+        reader = csv.DictReader(file)
     for row in reader:
         pizzas.append(row)
+except FileNotFoundError:
+    sys.exit("File does not exist")
 
 print(tabulate(pizzas, headers="keys", tablefmt="grid"))
