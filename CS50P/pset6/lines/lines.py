befe64acc684058785ab70a9c38f035624969ad9
walread <will.alread@gmail.com>
2022-09-17 21:49:46 +0000

    Sat, Sep 17, 2022, 9:49 PM +00:00

---
+++ CS50P/pset6/lines/lines.py
@@ -0,0 +1,17 @@
+import sys
+
+lines = []
+
+try:
+    with open(sys.argv[1]) as file:
+        for line in file:
+            lines.append(line.lstrip())
+except FileNotFoundError:
+    sys.exit
+
+counter = 0
+for line in lines:
+    if not lines[line].startswith("#"):
+        counter += 1
+
+print(counter)
