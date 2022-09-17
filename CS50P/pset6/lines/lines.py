c858b6b1af86a17f766af2658a17e65aaae77384
walread <will.alread@gmail.com>
2022-09-17 22:06:12 +0000

    Sat, Sep 17, 2022, 10:06 PM +00:00

--- CS50P/pset6/lines/lines.py
+++ CS50P/pset6/lines/lines.py
@@ -6,13 +6,16 @@ if len(sys.argv) < 2:
     sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 2:
     sys.exit("Too many command-line arguments")
+name, type = sys.argv[1].split(".")
+if type != "py"
+    sys.exit("Not a Python file")
 
 try:
     with open(sys.argv[1]) as file:
         for line in file:
             lines.append(line.lstrip())
 except FileNotFoundError:
-    sys.exit(")
+    sys.exit("File does not exist")
 
 counter = 0
 for line in lines:
