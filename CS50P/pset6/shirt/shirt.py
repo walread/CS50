f054f1c87992cba9a46c8fecd744fd2a77b88517
walread <will.alread@gmail.com>
2022-09-18 14:26:17 +0000

    Sun, Sep 18, 2022, 2:26 PM +00:00

--- CS50P/pset6/shirt/shirt.py
+++ CS50P/pset6/shirt/shirt.py
@@ -3,13 +3,15 @@ import sys
 input_file, input_type = sys.argv[1].split(".")
 output_file, output_type = sys.argv[2].split(".")
 
+
+
 if len(sys.argv) > 3:
     sys.exit("Too many command-line arguments")
 elif len(sys.argv) < 3:
     sys.exit("Too few command-line arguments")
-elif input_type != ("jpg" or "jpeg" or "png"):
+elif input_type != "jpg" or input_type != "jpeg" or input_type != "png":
     sys.exit("Invalid input")
-elif output_type != ("jpg" or "jpeg" or "png"):
+elif output_type != "jpg" or output_type != "jpeg" or output_type != "png":
     sys.exit("Invalid output")
 elif input_type != output_type:
     sys.exit("Input and output have different extensions")
