c207d96d2c839f7c9f7afb7d2d7c01b80c50b8b9
walread <will.alread@gmail.com>
2022-09-18 14:04:48 +0000

    shirt alteration

--- CS50P/pset6/shirt/shirt.py
+++ CS50P/pset6/shirt/shirt.py
@@ -4,3 +4,5 @@ if len(sys.argv) > 3:
     sys.exit("Too many command-line arguments")
 if len(sys.argv) < 3:
     sys.exit("Too few command-line arguments")
+input_file, input_type = sys.argv[1].split(".")
+input_file, input_type = sys.argv[2].split(".")
