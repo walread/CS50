d800d37c1b15b754bef240d35a905516caaa0eb0
walread <will.alread@gmail.com>
2022-09-18 11:59:16 +0000

    Sun, Sep 18, 2022, 11:59 AM +00:00

--- CS50P/pset6/scourgify/scourgify.py
+++ CS50P/pset6/scourgify/scourgify.py
@@ -5,12 +5,12 @@ if len(sys.argv) > 3:
 elif len(sys.argv) < 3:
     print("Too few command-line arguments")
 
+list = []
+
 try:
     with open(sys.argv[1]) as before:
         reader = csv.DictReader(before)
-        with open(sys.argv[2], "a") as after:
-            writer = csv.DictWriter(after)
-            for row in before:
+        for row in before:
+            list.append(row) 
 except FileNotFoundError:
     sys.exit(f"Cound not read {sys.argv[1]}")
-    
