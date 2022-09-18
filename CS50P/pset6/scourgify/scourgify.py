e265a3eab9d9589837134b2045365efa8b61fee1
walread <will.alread@gmail.com>
2022-09-18 12:08:15 +0000

    Sun, Sep 18, 2022, 12:08 PM +00:00

--- CS50P/pset6/scourgify/scourgify.py
+++ CS50P/pset6/scourgify/scourgify.py
@@ -11,7 +11,7 @@ try:
     with open(sys.argv[1]) as before:
         reader = csv.DictReader(before)
         for row in before:
-            first, last, house = row.split(",")
+            , last, house = row.split(",")
             list.append({"first": first.lstrip(), "last": last, "house": house})
 except FileNotFoundError:
     sys.exit(f"Cound not read {sys.argv[1]}")
