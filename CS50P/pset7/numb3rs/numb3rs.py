03361be93b62cfe4034aadf72c9d6f15b3d5c0d6
walread <will.alread@gmail.com>
2022-09-23 12:28:22 +0000

    Fri, Sep 23, 2022, 12:28 PM +00:00

--- CS50P/pset7/numb3rs/numb3rs.py
+++ CS50P/pset7/numb3rs/numb3rs.py
@@ -7,7 +7,7 @@ def main():
 
 
 def validate(ip):
-    if re.search(r"^[0-255]\.[0-255]\.[0-255]$", ip):
+    if re.search(r"^[0-2][0-5][0-5]\.[0-2][0-5][0-5]\.[0-2][0-5][0-5]$", ip):
         return "True"
     else:
         return "False"
