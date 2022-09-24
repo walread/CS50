adc1dfaa8e1f18321f31c4e537f644a7af6fd86b
walread <will.alread@gmail.com>
2022-09-24 10:33:08 +0000

    Sat, Sep 24, 2022, 10:33 AM +00:00

--- CS50P/pset7/numb3rs/test_numb3rs.py
+++ CS50P/pset7/numb3rs/test_numb3rs.py
@@ -2,6 +2,11 @@ from numb3rs import validate
 
 
 def test_valid():
+    assert validate("255.255.255.255") == True
+    assert validate("000.000.000.000") == True
 
 
 def test_invalid():
+    assert validate("0000.1.2.3") == False
+    assert validate("1.2.3.1000") == False
+    assert validate("cat") == False
