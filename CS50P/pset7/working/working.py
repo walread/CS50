a562f05b126a108c3ef1183176ccd746845aaf07
walread <will.alread@gmail.com>
2022-09-25 14:49:27 +0000

    Sun, Sep 25, 2022, 2:49 PM +00:00

--- CS50P/pset7/working/working.py
+++ CS50P/pset7/working/working.py
@@ -16,9 +16,9 @@ def convert(s):
         if matches.group(6) == "PM":
             matches.group(4) == int(matches.group(4))+12
         if None in matches.groups():
-            return (f"{matches.group(1):02}:00 to {matches.group(4):02}:00")
+            return (f"{matches.group(1)}:00 to {matches.group(4)}:00")
         else:
-            return (f"{matches.group(1):02}{matches.group(2)} to {matches.group(4):02}{matches.group(4)}")
+            return (f"{matches.group(1)}{matches.group(2)} to {matches.group(4)}{matches.group(5)}")
 
 
 if __name__ == "__main__":
