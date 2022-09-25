16f28771b7459eb82fe2d1271239d6b602ebf368
walread <will.alread@gmail.com>
2022-09-25 12:42:37 +0000

    Sun, Sep 25, 2022, 12:42 PM +00:00

--- CS50P/pset7/watch/watch.py
+++ CS50P/pset7/watch/watch.py
@@ -7,7 +7,7 @@ def main():
 
 
 def parse(s):
-    if matches := re.search(r'src="https?://(?:www\.)?youtube\.com/embed/(\w)*"', s):
+    if matches := re.search(r'src="https?://(?:www\.)?youtube\.com/embed/(\w*)"', s):
         return (f"https://youtu.be/{matches.group(1)}")
 
 
