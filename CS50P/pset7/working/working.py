afb818104264f5a07c18fd6033cc38c70def724c
walread <will.alread@gmail.com>
2022-09-25 13:17:11 +0000

    Sun, Sep 25, 2022, 1:17 PM +00:00

--- CS50P/pset7/working/working.py
+++ CS50P/pset7/working/working.py
@@ -7,7 +7,7 @@ def main():
 
 
 def convert(s):
-    if matches := re.search(r"^[]:[] ((AM)|(PM)) to []:[] ((AM)|(PM))$", s)
+    if matches := re.search(r"^[1]?[0-9]:[] ((AM)|(PM)) to []:[] ((AM)|(PM))$", s)
         return matches.group(1)
 
 
