5c1766ea5dc46cf6671918d98fef3260fcc28128
walread <will.alread@gmail.com>
2022-09-25 13:43:22 +0000

    Sun, Sep 25, 2022, 1:43 PM +00:00

--- CS50P/pset7/working/working.py
+++ CS50P/pset7/working/working.py
@@ -8,7 +8,7 @@ def main():
 
 def convert(s):
     matches = re.search(r"^(1[0-2]|[0-9])(:[0-5][0-9])? (AM|PM) to (1[0-2]|[0-9])(:[0-5][0-9])? (AM|PM)$", s)
-    if matches.group(1, 3, 4, 6) == None:
+    if not matches.groups():
         raise ValueError
     else:
         return matches.groups()
