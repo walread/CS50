dcb53bb15a7cddea91686f054429c10e6f2b12ab
walread <will.alread@gmail.com>
2022-09-25 14:16:37 +0000

    Sun, Sep 25, 2022, 2:16 PM +00:00

--- CS50P/pset7/working/working.py
+++ CS50P/pset7/working/working.py
@@ -8,13 +8,10 @@ def main():
 
 def convert(s):
     matches = re.search(r"^(1[0-2]|[0-9])(:[0-5][0-9])? (AM|PM) to (1[0-2]|[0-9])(:[0-5][0-9])? (AM|PM)$", s)
-    try:
-        if None in matches.group(2):
-            return "None found"
-        else:
-            return matches.group(2)
-    except AttributeError:
-        raise ValueError
+    if None in matches.groups():
+        return matches.group(2)
+    else:
+        return matches.groups(2)
 
 
 if __name__ == "__main__":
