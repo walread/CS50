e109931aee309c878577f6a1e568c8d7ac216afb
walread <will.alread@gmail.com>
2022-09-26 11:02:13 +0000

    Mon, Sep 26, 2022, 11:02 AM +00:00

--- CS50P/pset7/working/working.py
+++ CS50P/pset7/working/working.py
@@ -12,9 +12,9 @@ def convert(s):
         raise ValueError
     else:
         if match.group(3) == "PM":
-            match.group(1) == int(match.group(1))+12
+            start = int(match.group(1))+12
         if match.group(6) == "PM":
-            match.group(4) == int(match.group(4))+12
+            end = int(match.group(4))+12
         if None in match.groups():
             return (f"{match.group(1)}:00 to {match.group(4)}:00")
         else:
