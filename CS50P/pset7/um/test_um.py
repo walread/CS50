88df55c6a759fb075e919cabd003f192ce6a6582
walread <will.alread@gmail.com>
2022-09-27 11:45:51 +0000

    Tue, Sep 27, 2022, 11:45 AM +00:00

--- CS50P/pset7/um/test_um.py
+++ CS50P/pset7/um/test_um.py
@@ -1,1 +1,11 @@
 from um import count
+
+def test_inwords():
+    assert count("yummy") == None
+
+def test_space():
+    assert count("um") == 1
+
+
+def test_case():
+    assert count("Um, thanks for the album.") == 1
