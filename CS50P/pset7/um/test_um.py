07915da090e34a4117d5934c4dbe32a49fe61951
walread <will.alread@gmail.com>
2022-09-27 11:53:08 +0000

    Tue, Sep 27, 2022, 11:53 AM +00:00

--- CS50P/pset7/um/test_um.py
+++ CS50P/pset7/um/test_um.py
@@ -1,11 +1,16 @@
 from um import count
 
-def test_inwords():
-    assert count("yummy") == None
 
 def test_space():
     assert count("um") == 1
+    assert count("Hello, um, world") == 1
+    assert count("This is, um... CS50.") == 1
 
 
 def test_case():
-    assert count("UM") == 1
+    assert count("Um... what are regular expressions") == 1
+    assert count("Um, thanks, um, regular expressions make sense now.") == 1
+
+
+def test_inwords():
+    assert count("Um? Mum? Is this that album where, um, umm, the clumsy alums play drums?") == None
