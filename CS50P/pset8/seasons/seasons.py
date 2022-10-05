f17cb08c3e43560012d1950a942109fbe34ba071
walread <will.alread@gmail.com>
2022-10-05 14:16:55 +0000

    Wed, Oct 5, 2022, 2:16 PM +00:00

--- CS50P/pset8/seasons/seasons.py
+++ CS50P/pset8/seasons/seasons.py
@@ -8,8 +8,8 @@ def main():
         birth = date.fromisoformat(input("Date of Birth: "))
     except ValueError:
         sys.exit("Invalid date")
-    time = (date.today() - birth)
-    print(type(time))
+    time = (date.today() - birth).seconds()
+    print(time)
 
 if __name__ == "__main__":
     main()
