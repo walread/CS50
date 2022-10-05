aa06ad6d81cb0a095639b25ab86833d916900ade
walread <will.alread@gmail.com>
2022-10-05 13:37:20 +0000

    Wed, Oct 5, 2022, 1:37 PM +00:00

--- CS50P/pset8/seasons/seasons.py
+++ CS50P/pset8/seasons/seasons.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, timedelta
 import sys
 
 
@@ -7,7 +7,7 @@ def main():
         birth = date.fromisoformat(input("Date of Birth: "))
     except ValueError:
         sys.exit("Invalid date")
-    time = date.today() - birth
+    time = total_seconds(date.today() - birth)
     print(time)
 
 if __name__ == "__main__":
