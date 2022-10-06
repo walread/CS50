be54514a57a1788c869892ff3557c81159efc5d8
walread <will.alread@gmail.com>
2022-10-06 12:20:49 +0000

    Thu, Oct 6, 2022, 12:20 PM +00:00

--- CS50P/pset8/jar/jar.py
+++ CS50P/pset8/jar/jar.py
@@ -1,6 +1,7 @@
 class Jar:
     def __init__(self, capacity=12):
-        ...
+        if capacity < 0:
+            raise ValueError("Capacity must be a non-nagative int")
 
     def __str__(self):
         ...
