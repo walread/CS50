29753de53d5238f0d16ce1a904840e78a493bdb3
walread <will.alread@gmail.com>
2022-10-07 10:44:38 +0000

    Fri, Oct 7, 2022, 10:44 AM +00:00

--- CS50P/pset8/jar/jar.py
+++ CS50P/pset8/jar/jar.py
@@ -24,3 +24,13 @@ class Jar:
     @property
     def size(self):
         return self.size
+
+
+def main():
+    jar = Jar()
+    jar.deposit(input("Deposit: "))
+    jar.withdraw(input("Withdraw: "))
+    print(jar.size)
+
+    if __name__ == "__main__":
+        main()
